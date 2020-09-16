455fb99250d9d911df127c4a85b9117214f7230e
Abinash S <s.abinash@outlook.com>
2020-09-16 21:26:26 +0530

    DB Update

--- trainreservation.sql
+++ trainreservation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 16, 2020 at 03:46 PM
+-- Generation Time: Sep 16, 2020 at 03:56 PM
 -- Server version: 8.0.18
 -- PHP Version: 7.4.0
 
@@ -30,16 +30,16 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF NOT EXISTS `reservation` (
-  `PNR_No` varchar(12) NOT NULL,
+  `PNR_No` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Train No` int(11) NOT NULL,
-  `Train Name` varchar(30) NOT NULL,
-  `Class` varchar(4) NOT NULL,
+  `Train Name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Class` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Date of Journey` date NOT NULL,
-  `Arrival` varchar(15) NOT NULL,
-  `Departure` varchar(15) NOT NULL,
-  `Boarding at` varchar(15) NOT NULL,
+  `Arrival` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Departure` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Boarding at` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`PNR_No`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
